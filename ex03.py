e4c3061de8574a716bb572b905d6b82869ff98c6
TheusLopes01 <mslopesilva01@gmail.com>
2023-10-31 22:55:25 -0300

    2.0

---
+++ ex03.py
@@ -0,0 +1,5 @@
+n = input('digite algo: ')
+print('o tipo primitivo desse valor é ', type(n))
+print('é um alfanumérico?', n.isalnum())
+print('é um número?', n.isnumeric())
+print('está capitalizada?', n.isupper())
