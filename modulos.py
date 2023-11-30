7bfaedfa0017e1d67097fb4a63fbf96e33d1db39
TheusLopes01 <mslopesilva01@gmail.com>
2023-11-29 21:29:10 -0300

    updatestudy

---
+++ modulos.py
@@ -0,0 +1,7 @@
+from math import sqrt, floor
+# import math
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+# print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
